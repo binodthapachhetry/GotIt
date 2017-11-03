87ef8b8928447eed2c49a1df5793a3e5b5cd5279
Josh Miller <miller.josh@husky.neu.edu>
2017-11-03 12:01:29 -0400

    questions stand out more

--- GotIt_ChromeExt/myscript.js
+++ GotIt_ChromeExt/myscript.js
@@ -12,6 +12,7 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
         console.log(sentence_ar.length)
 
         if(sentence_ar.length>3){ // skip any paragraph with fewer than 3 lines
+									//TODO also skip if sum of sentences is fewer than 50 words (helps avoid other edge cases)
 
             butNum++;
 
@@ -25,23 +26,27 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
             count = count + 1;
             myBodyElements[i].setAttribute("data-intro","Get it, use it.");
 
-            //
+            // ---- NLP
+			// TODO JOSH - instead, find a sentence in the paragraph that makes a good question
             if(nlp(sentence_ar[0]).clauses().data().length==1){
                 verbCount = nlp(sentence_ar[0]).verbs().data()
                 // use nlp compromise library to check if the first verb occurence is negative
                 negCount = nlp(sentence_ar[0]).verbs(0).isNegative().length
                 if(negCount==1){
                     // if negative make it positive
-                    questtion = nlp(sentence_ar[0]).sentences(0).toPositive().out()
+                    question = nlp(sentence_ar[0]).sentences(0).toPositive().out()
                 }else{
                     // if positive make it negative
                     question = nlp(sentence_ar[0]).verbs(0).toNegative().out()
                 }
+			// ---- /NLP
                 
                 console.log(question)
                 var log = true;
                 // create a button element
-                var btn = document.createElement("button")
+                var btn = document.createElement("button");
+				btn.style.background='#000000'; //document.getElementById("button") ?
+				btn.style.color = '#FFFFFF';
                 var t = document.createTextNode("?");
                 btn.setAttribute('id',bid);
                 btn.setAttribute('class','confirm')
@@ -69,6 +74,8 @@ function generate(qu,sen,log,bid,id){
   document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.replace(sen,new_sen)
   var j_id = "#"+id+" span"
   $(j_id).css('background-color', 'black');
+  //TODO also change text color to black, so that it can't be seen by highlight
+  //TODO collect the original text color and background color, may not be black on white
 
 var j_bid = "#"+bid
 
@@ -80,7 +87,7 @@ $.confirm({
     title: "True or False",
     confirm: function(button) {
         console.log('pressed confirm');
-        $(j_id).css('background-color', 'white'); // unhide the sentence
+        $(j_id).css('background-color', 'white'); // unhide the sentence TODO also return text to original color
         $(j_bid).remove(); // remove the question mark
     },
     cancel: function(button) {
