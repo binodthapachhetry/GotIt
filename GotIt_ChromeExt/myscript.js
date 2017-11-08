b13523b16d285d4ceb209cb3897aae3ed5f14617
Josh Miller <miller.josh@husky.neu.edu>
2017-11-08 13:25:26 -0500

    more nlp updates

--- GotIt_ChromeExt/myscript.js
+++ GotIt_ChromeExt/myscript.js
@@ -31,13 +31,13 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 		
 		var potential_sentences = [];
 		
-		var good_sentence = /(.*)([Cc]ause|[Ss]ymptoms|safe|[Tt]reatment|[Uu]se|([Ss]ee|[Cc]all|[Cc]onsult|[Aa]sk) (a|your) doctor if|[Cc]all 911 if|[Ss]ide effect|percent)(.*)/;
+		var good_sentence = /(.*)([Cc]ause|[Ss]ymptoms|safe|[Tt]reatment|([Ss]ee|[Cc]all|[Cc]onsult|[Aa]sk) (a|your) doctor if|[Cc]all 911 if|[Ss]ide effect|percent)(.*)/;
 		
 		var q_found = false; //1 question per paragraph
 		var match_counts = [];
 		
 		for (ii = 0; ii < sentence_ar.length; ii++){
-			if (sentence_ar[ii].length > 40 && sentence_ar[ii].length < 200){ //reasonably sized sentences
+			if (sentence_ar[ii].length > 45 && sentence_ar[ii].length < 250){ //reasonably sized sentences
 			
 				if (good_sentence.exec(sentence_ar[ii]) != null){
 						potential_sentences.push(sentence_ar[ii]);
@@ -56,53 +56,197 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 		var sentence, question;
 		var reverse = (Math.floor(Math.random() * 2) == 0); //whether false is true, chosen randomly; if reverese=true, then false is correct answer
 		potential_sentences.forEach(function(s){
+			var skip = false;
+			if (q_found || /:/.exec(s) != null){
+				//skip if we already found a question or this sentence has a colon
+				skip = true;
+			}
 			
+			//be wary of cause/symptom sentences - extra check to make sure they are okay
+			if (/[Cc]ause/.exec(s) != null && /[Ss]ymptom/.exec(s) != null){
+				skip = true;
+			}
+			if ((/[Cc]ause/.exec(s) != null || /[Ss]ymptom/.exec(s) != null) && (s.length < 60 || s.length > 150)){
+				skip = true;
+			}
 			
-			if (!q_found && /percent/.exec(s) != null){ //percents are always a good question to reverse
+			if (!skip && /viral/.exec(s) != null){ //good question to reverse
 				reverse = true;
 				if (reverse) {
-					console.log("trying to reverse percent.");
+					sentence = s;
+					sentence = sentence.replace("viral", "bacterial");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED viral: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /bacterial/.exec(s) != null){ //good question to reverse
+				reverse = true;
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("bacterial", "viral");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED bacterial: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /to \d\d?\d? percent/.exec(s) != null){ //percents are always a good question to reverse
+				reverse = true;
+				if (reverse) {
 					//move around the percents so the statement is false -- if no percents found, skip this question
 					if (/(\D\d\d?\d?)\D/.exec(s) != null) {
-						matches = /\D(\d\d?\d?)\D/.exec(s);
+						matches = /\D(\d\d?\d?) to (\d\d?\d?) percent/.exec(s);
 						if (matches[1] > 0 && matches[1] < 100) {
-							console.log("reverse percent");
 							sentence = s;
-							sentence.replace(matches[1], (matches[1] + 20 + Math.floor(Math.random() * 40)) % 100); //tweak percent to make wrong
-							q_found = true;
-							if (matches.length > 2) {
-								sentence.replace(matches[2], (matches[2] + 20 + Math.floor(Math.random() * 40)) % 100); //if there's another percent, also tweak
+							new_num1 = ((matches[1] - 30 + Math.floor(Math.random() * 50)) % 100);
+							while (new_num1 % 5 != 0){
+								new_num1++;
 							}
+							sentence = sentence.replace(new RegExp(matches[1]), new_num1.toString()); //tweak percent to make wrong
+							
+							new_num2 = ((matches[2] - 50 + Math.floor(Math.random() * 70)) % 100);
+							while (new_num2 % 5 != 0 || new_num2 < new_num2+5 ){
+								new_num++;
+							}
+							sentence = sentence.replace(new RegExp(matches[2]), new_num2.toString()); //tweak percent to make wrong
+							
+							q_found = true;
+							skip = true;
+							console.log("REVERSED percent: " + sentence);
 						}
 					}
 				}
 				else {
 					sentence = s;
 					q_found = true;
+					skip = true;
 				}
 			}
 			
-			if (!q_found && /\sunsafe/.exec(s) != null) {
+			if (!skip && /percent/.exec(s) != null){ //percents are always a good question to reverse
+				reverse = true;
 				if (reverse) {
-					sentence = s;
-					sentence.replace("unsafe", "safe");
-					q_found = true;
+					//move around the percents so the statement is false -- if no percents found, skip this question
+					if (/(\D\d\d?\d?)\D/.exec(s) != null) {
+						matches = /\D(\d\d?\d?)\D/.exec(s);
+						if (matches[1] > 0 && matches[1] < 100) {
+							sentence = s;
+							new_num = ((matches[1] + 20 + Math.floor(Math.random() * 40)) % 100);
+							while (new_num % 5 != 0){
+								new_num++;
+							}
+							sentence = sentence.replace(new RegExp(matches[1]), new_num.toString()); //tweak percent to make wrong
+							q_found = true;
+							skip = true;
+							console.log("REVERSED percent: " + sentence);
+						}
+					}
 				}
 				else {
 					sentence = s;
 					q_found = true;
+					skip = true;
 				}
 			}
 			
-			if (!q_found && /\ssafe/.exec(s) != null) {
+				//TODO if there are digits, change to other digits
+			if (!skip && /\d/.exec(s) != null){
+				//move around some digits, good question to reverse
+				reverse = true;
 				if (reverse) {
 					sentence = s;
-					sentence.replace("safe", "unsafe");
-					q_found = true;
+					sentence = sentence.replace("30s", "20s");
+					sentence = sentence.replace("60s", "30s");
+					sentence = sentence.replace("40s", "60s");
+					digit_matches = /([1-9])[^0s]/.exec(sentence);
+					if (digit_matches != null) {
+						for (i = 1; i < digit_matches.length; i++){
+							sentence = sentence.replace(digit_matches[i], Math.ceil(Math.random()*10).toString());
+						}
+						
+						if (sentence != s) { //some change was made
+							console.log("REVERSED digit: " + sentence);
+							q_found = true;
+							skip = true;
+						}
+					}
 				}
 				else {
 					sentence = s;
 					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /\sunsafe/.exec(s) != null) {
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("unsafe", "safe");
+					console.log("REVERSED unsafe: " + sentence);
+					q_found = true;
+					skip = true;
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /\ssafe/.exec(s) != null) {
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("safe", "unsafe");
+					console.log("REVERSED safe: " + sentence);
+					q_found = true;
+					skip = true;
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			//if everything has failed up to this point, with a 10% chance make this a simple negated sentence
+			if (!skip && Math.random() < .1){
+				if (reverse){
+					sentence = s;
+					if(nlp(sentence).clauses().data().length==1){
+						verbCount = nlp(sentence).verbs().data();
+						// use nlp compromise library to check if the first verb occurence is negative
+						negCount = nlp(sentence).verbs(0).isNegative().length;
+						if(negCount==1){
+							// if negative make it positive
+							sentence = nlp(sentence).sentences(0).toPositive().out();
+							console.log("REVERSED positive: " + sentence);
+							q_found = true;
+							skip = true;
+						}else{
+							// if positive make it negative
+							sentence = nlp(sentence).verbs(0).toNegative().out();
+							console.log("REVERSED negative: " + sentence);
+							q_found = true;
+							skip = true;
+						}
+					}
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
 				}
 			}
 			
@@ -193,7 +337,6 @@ $.confirm({
         console.log('pressed confirm');
         $(j_id).css('background-color', 'white');
         $(j_id).css('text-decoration','underline');
-        $(j_bid).remove(); // remove the question mark on correct        
     },
     cancel: function(button) {
         console.log('pressed cancel');
@@ -221,7 +364,6 @@ $.confirm({
         console.log('pressed cancel');
         $(j_id).css('background-color', 'white'); // unhide the sentence
         $(j_id).css('text-decoration','underline'); // unhide the sentence
-        $(j_bid).remove(); // remove the question mark on correct
         
 
     },
