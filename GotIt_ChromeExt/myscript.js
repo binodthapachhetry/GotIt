bfa051458ea95cc7700e3dcae263b0e1aaa2fba1
Josh Miller <miller.josh@husky.neu.edu>
2017-11-09 13:08:37 -0500

    nlp bug fixes

--- GotIt_ChromeExt/myscript.js
+++ GotIt_ChromeExt/myscript.js
@@ -3,7 +3,7 @@ var docBody = document.getElementsByTagName("body")[0]; // read document
 var myBodyElements = docBody.getElementsByTagName("p"); // read paragraphs
 var arr=[]
 
-
+var pos_count = 0;
 var count = 1;
 butNum = 0;
 for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
@@ -34,13 +34,12 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 		var potential_sentences = [];
 		var potential_sentences_index = [];
 		
-		var good_sentence = /(.*)(\d|[Cc]ause|viral|bacterial|chronic|acute|[Ss]ymptom|safe|%|([Ss]ee|[Cc]all|[Cc]onsult|[Aa]sk) (a|your) doctor if|[Cc]all 911 if|[Ss]ide effect|percent)(.*)/;
+		var good_sentence = /(.*)(\d|[Cc]ause|viral|always|never|over|under|bacterial|chronic|acute|[Ss]ymptom|safe|%|([Ss]ee|[Cc]all|[Cc]onsult|[Aa]sk) (a|your) doctor if|[Cc]all 911 if|[Ss]ide effect|percent)(.*)/;
 		
 		var q_found = false; //1 question per paragraph
 		var match_counts = [];
 		
 		for (ii = 0; ii < sentence_ar.length; ii++){
-			//console.log("outer loop " + ii);
 			if (sentence_ar[ii].length > 45 && sentence_ar[ii].length < 250){ //reasonably sized sentences
 			
 				if (good_sentence.exec(sentence_ar[ii]) != null){
@@ -60,21 +59,24 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 		
 		var sentence, question;
 		var reverse = (Math.floor(Math.random() * 2) == 0); //whether false is true, chosen randomly; if reverse=true, then false is correct answer
+		if (pos_count > 3){
+			reverse = true;
+		}
 		// potential_sentences.forEach(function(s){
 		potential_sentences_index.forEach(function(i){
-			//console.log("Looping through " + i);
+			console.log("pos count " + pos_count);
 			s = potential_sentences[i];		
 			var skip = false;
-			if (q_found || /:/.exec(s) != null){
+			if (q_found || /(.*):(.*)/.exec(s) != null){
 				//skip if we already found a question or this sentence has a colon
 				skip = true;
 			}
 			
 			//be wary of cause/symptom sentences - extra check to make sure they are okay
-			if (/[Cc]ause/.exec(s) != null && /[Ss]ymptom/.exec(s) != null){
+			if (/(.*)[Cc]ause(.*)/.exec(s) != null && /(.*)[Ss]ymptom(.*)/.exec(s) != null){
 				skip = true;
 			}
-			if ((/[Cc]ause/.exec(s) != null || /[Ss]ymptom/.exec(s) != null) && (s.length < 60 || s.length > 150)){
+			if ((/(.*)[Cc]ause(.*)/.exec(s) != null || /(.*)[Ss]ymptom(.*)/.exec(s) != null) && (s.length < 60 || s.length > 150)){
 				skip = true;
 			}
 			
@@ -142,18 +144,81 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 				}
 			}
 			
+			if (!skip && /(.*)always(.*)/.exec(s) != null){ //good question to reverse
+				reverse = true;
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("always", "never");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED always: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /(.*)never(.*)/.exec(s) != null){ //good question to reverse
+				reverse = true;
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("never", "always");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED never: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /(.*)over(.*)/.exec(s) != null){ //good question to reverse
+				reverse = true;
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("over", "under");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED over: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
+			if (!skip && /(.*)under(.*)/.exec(s) != null){ //good question to reverse
+				reverse = true;
+				if (reverse) {
+					sentence = s;
+					sentence = sentence.replace("under", "over");
+					q_found = true;
+					skip = true;
+					console.log("REVERSED under: " + sentence);
+				}
+				else {
+					sentence = s;
+					q_found = true;
+					skip = true;
+				}
+			}
+			
 			if (!skip && /(.*)to \d\d?\d?(%| percent)(.*)/.exec(s) != null){ //percents are always a good question to reverse
 				reverse = true;
 				if (reverse) {
 					//move around the percents so the statement is false -- if no percents found, skip this question
-					if (/.*(\D\d\d?\d?)\D.*/.exec(s) != null) {
+					if (/.*(\D\d\d?\d?).*/.exec(s) != null) {
 						matches = /.*\D(\d\d?\d?) to (\d\d?\d?)(%| percent).*/.exec(s);
 						if (matches[1] > 0 && matches[1] < 100) {
 							sentence = s;
 							new_num1 = ((matches[1] - 30 + Math.floor(Math.random() * 50)) % 100);
 							while (new_num1 % 5 != 0){
 								new_num1++;
-								console.log(".");
 								if (new_num1 > 90) {
 									(matches[1] - 60 + Math.floor(Math.random() * 50)) % 100;
 								}
@@ -163,7 +228,6 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 							new_num2 = ((matches[2] - 50 + Math.floor(Math.random() * 70)) % 100);
 							while (new_num2 % 5 != 0 || new_num1 + 5 > new_num2 ){
 								new_num2 = new_num2 + 1;
-								console.log(".");
 								if (new_num2 > 100) {
 									new_num2 = new_num2 - 10;
 								}
@@ -187,8 +251,8 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 				reverse = true;
 				if (reverse) {
 					//move around the percents so the statement is false -- if no percents found, skip this question
-					if (/.*(\D\d\d?\d?)\D.*/.exec(s) != null) {
-						matches = /.*\D(\d\d?\d?)\D.*/.exec(s);
+					if (/.*(\D\d\d?\d?).*/.exec(s) != null) {
+						matches = /.*\D(\d\d?\d?).*/.exec(s);
 						if (matches[1] > 0 && matches[1] < 100) {
 							sentence = s;
 							new_num = ((matches[1] + 20 + Math.floor(Math.random() * 40)) % 100);
@@ -218,10 +282,10 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 					sentence = sentence.replace("30s", "20s");
 					sentence = sentence.replace("60s", "30s");
 					sentence = sentence.replace("40s", "60s");
-					digit_matches = /([1-9])[^0s]/.exec(sentence);
+					digit_matches = /([1-9])/.exec(sentence);
 					if (digit_matches != null) {
 						for (ii = 1; ii < digit_matches.length; ii++){
-							sentence = sentence.replace(digit_matches[ii], Math.ceil(Math.random()*10).toString());
+							sentence = sentence.replace(digit_matches[ii], Math.ceil(Math.random()*9).toString());
 						}
 						
 						if (sentence != s) { //some change was made
@@ -239,6 +303,7 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 			}
 			
 			if (!skip && /(.*)\sunsafe(.*)/.exec(s) != null) {
+				reverse = true;
 				if (reverse) {
 					sentence = s;
 					sentence = sentence.replace("unsafe", "safe");
@@ -254,6 +319,7 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 			}
 			
 			if (!skip && /(.*)\ssafe(.*)/.exec(s) != null) {
+				reverse = true;
 				if (reverse) {
 					sentence = s;
 					sentence = sentence.replace("safe", "unsafe");
@@ -268,70 +334,59 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 				}
 			}
 			
-			//if everything has failed up to this point, with a 20% chance make this a simple negated sentence
-			if (!skip && Math.random() < .2){
-				if (reverse){
-					sentence = s;
-					if(nlp(sentence).clauses().data().length==1){
-						verbCount = nlp(sentence).verbs().data();
-						// use nlp compromise library to check if the first verb occurence is negative
-						negCount = nlp(sentence).verbs(0).isNegative().length;
-						if(negCount==1){
-							// if negative make it positive
-							sentence = nlp(sentence).sentences(0).toPositive().out();
-							console.log("REVERSED positive: " + sentence);
-							q_found = true;
-							skip = true;
-						}else{
-							// if positive make it negative
-							sentence = nlp(sentence).verbs(0).toNegative().out();
-							console.log("REVERSED negative: " + sentence);
-							q_found = true;
-							skip = true;
-						}
-					}
-				}
-				else {
-					sentence = s;
-					q_found = true;
-					skip = true;
-				}
-			}
 
 			if(q_found){
 				index = i;
 			}
 			
-			/*
-			if (!q_found && /include/.exec(s) != null) { //there might be a list
-				if (/(.*),(.*), (and|or) (.*)/.exec(s) != null) {
-					if (reverse) {
-						sentence = s;
-						matches = /(.*)(includ.*\s)(.*,)(.*,)(and|or)(.*)/.exec(s);
-						if (matches.length > 5) {
-							sentence.replace(matches[5], "but not");
-							q_found = true;
-						}
-					}
-					else {
-						sentence = s;
+		}); //end of foreach sentence
+		
+		if (pos_count > 5) {
+			//just skip this paragraph, nothing interesting
+			continue;
+		}
+		
+		if (!q_found || sentence == null){ //couldn't find a question up to this point, just pick a good sentence and maybe negate it
+			var randomNum = Math.floor(Math.random() * potential_sentences.length);
+			sentence = potential_sentences[randomNum];
+			index = potential_sentences_index[randomNum];		
+			// sentence = potential_sentences[Math.floor(Math.random() * potential_sentences.length)];
+			//if everything has failed up to this point make a random sentence a simple negated sentence
+			if (reverse && Math.random() < .2){
+				if(nlp(sentence).clauses().data().length==1){
+					verbCount = nlp(sentence).verbs().data();
+					// use nlp compromise library to check if the first verb occurence is negative
+					negCount = nlp(sentence).verbs(0).isNegative().length;
+					if(negCount==1){
+						// if negative make it positive
+						sentence = nlp(sentence).sentences(0).toPositive().out();
+						console.log("REVERSED positive: " + sentence);
 						q_found = true;
+						skip = true;
+					}else{
+						// if positive make it negative
+						sentence = nlp(sentence).verbs(0).toNegative().out();
+						console.log("REVERSED negative: " + sentence);
+						q_found = true;
+						skip = true;
 					}
 				}
-				*/
-			// }
-		});
+			}
+			else {
+				reverse = false;
+				q_found = true;
+				skip = true;
+			}
+		}
 		
-		if (!q_found){ //couldn't find a question up to this point, just pick a good sentence and make it a true question
-			var randomNum = Math.floor(Math.random() * potential_sentences.length)
-			sentence = potential_sentences[randomNum];
-			index = potential_sentences_index[randomNum]			
-			// sentence = potential_sentences[Math.floor(Math.random() * potential_sentences.length)];
-			reverse = false;
+		if (reverse == false) {
+			pos_count++;
+		}
+		else {
+			pos_count--;
 		}
 		
 		question = sentence + ".";
-		console.log(question)
 		var log = true;
 		// create a button element
 		var btn = document.createElement("button");
@@ -372,7 +427,6 @@ for (i = 0; i < myBodyElements.length; i++) { // loop over each paragraph
 
 // hide the sentence in the paragraph while question is generated
 function generate(qu,sen,log,bid,id,isReverse){
-  console.log(sen);
   var new_sen = "<span>"+sen+"</span>"
   document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.replace(sen,new_sen)
   var j_id = "#"+id+" span"
@@ -392,6 +446,7 @@ $.confirm({
         console.log('pressed confirm');
         $(j_id).css('background-color', 'white');
         $(j_id).css('text-decoration','underline');
+		$(j_bid).remove(); // remove the question mark on incorrect
     },
     cancel: function(button) {
         console.log('pressed cancel');
@@ -419,6 +474,8 @@ $.confirm({
         console.log('pressed cancel');
         $(j_id).css('background-color', 'white'); // unhide the sentence
         $(j_id).css('text-decoration','underline'); // unhide the sentence
+		$(j_bid).remove(); // remove the question mark on incorrect
+
         
 
     },
