73fda8cc05e863c1a21b40f464e4a2a695af5bdb
Binod Thapa Chhetry <binod.thapachhetry@gmail.com>
2017-11-13 13:36:09 -0500

    show actual instruction video for the app from the broswer action menu

--- GotIt_ChromeExt/popup.js
+++ GotIt_ChromeExt/popup.js
@@ -1,4 +1,4 @@
 function count(){
-    document.getElementById('demo').src = chrome.extension.getURL("big_buck_bunny.mp4"); 
+    document.getElementById('demo').src = chrome.extension.getURL("got_itinstruction.mp4"); 
 }
 document.getElementById('do-count').onclick = count;
